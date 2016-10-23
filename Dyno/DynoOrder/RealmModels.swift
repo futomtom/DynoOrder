dc41924c5e1c1d99ffc1235739f1ddf1d4ea5979
alexfu <afu@dynosense.com>
2016-10-23 00:17:16 -0700

    e

--- Dyno/DynoOrder/RealmModels.swift
+++ Dyno/DynoOrder/RealmModels.swift
@@ -1,8 +1,22 @@
 import Foundation
 import RealmSwift
 
-class test: Object {
+class order: Object {
+  dynamic var order_id = ""
+  let items = List<products>
+}
+
+class products: Object {
+  dynamic var pid = 0
   dynamic var name = ""
+  dynamic var price = 0
+  dynamic var type = 0
+}
+
+class sales: Object {
+  dynamic var name = ""
+    dynamic var birthdate:NSDate
+    dynamic var name = ""
   dynamic var price  = 0
 }
 
